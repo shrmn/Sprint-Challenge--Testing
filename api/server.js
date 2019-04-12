88163286cfc34e2697fe90fcd47aaf3b8115111b
Dan Sherman <dvsherman@protonmail.com>
2019-04-12 10:29:18 -0700

    wrote tests for GET on server.test.js

--- api/server.js
+++ api/server.js
@@ -15,11 +15,11 @@ server.get("/", async (req, res) => {
 server.post("/api/games", async (req, res) => {
   const videogame = req.body;
   const { title, genre } = videogame;
-  // if (!title || !genre) {
-  //   return res
-  //     .status(422)
-  //     .json({ message: "Both a name and a genre are required" });
-  // }
+  if (!title || !genre) {
+    return res
+      .status(422)
+      .json({ message: "Both a name and a genre are required" });
+  }
   try {
     const newVideogame = await vgModel.insert(videogame);
     res.status(201).json(videogame);
@@ -27,6 +27,17 @@ server.post("/api/games", async (req, res) => {
     res.status(500).json({
       error: `Adding new video game failed because ${error}`
     });
+  }
+});
+
+server.get("/api/games", async (req, res) => {
+  try {
+    const videogames = await vgModel.get();
+    res.status(200).json(videogames);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Unable to retrieve videogames: ${error}` });
   }
 });
 
--- api/server.test.js
+++ api/server.test.js
@@ -2,11 +2,23 @@ const request = require("supertest");
 const server = require("./server");
 const db = require("../data/dbConfig");
 
+const videogame1 = {
+  title: "Megaman",
+  genre: "platformer"
+};
+const videogame2 = {
+  title: "Super Mario Sunshine",
+  genre: "platformer"
+};
+const videogame3 = {
+  title: "The Legend of Zelda: Breath of the Wild",
+  genre: "RPG"
+};
+
 describe("server.js", () => {
   afterEach(async () => {
     await db("videogames").truncate();
   });
-
   describe("sanity check", () => {
     it("should return status 200", async () => {
       const response = await request(server).get("/");
@@ -20,7 +32,7 @@ describe("server.js", () => {
       });
     });
   });
-  describe("POST /games", () => {
+  describe("POST /api/games", () => {
     it("should respond with status code 201 on success", async () => {
       const videogame = {
         title: "Pacman",
@@ -35,6 +47,60 @@ describe("server.js", () => {
         genre: "Arcade",
         releaseYear: 1980
       });
+    });
+
+    it("should return status code 422 if title missing", async () => {
+      const vgMissingTitle = {
+        genre: "Arcade",
+        releaseYear: 1980
+      };
+      const response = await request(server)
+        .post("/api/games")
+        .send(vgMissingTitle);
+      expect(response.status).toBe(422);
+    });
+
+    it("should return status code 422 if genre missing", async () => {
+      const vgMissingGenre = {
+        title: "Pacman",
+        releaseYear: 1980
+      };
+      const response = await request(server)
+        .post("/api/games")
+        .send(vgMissingGenre);
+      expect(response.status).toBe(422);
+    });
+  });
+
+  describe("GET /api/games", () => {
+    it("should return array of games", async () => {
+      let response = await request(server).get("/api/games");
+
+      expect(response.body).toHaveLength(0);
+
+      await request(server)
+        .post("/api/games")
+        .send(videogame1);
+
+      response = await request(server).get("/api/games");
+
+      expect(response.body).toHaveLength(1);
+    });
+
+    it("should return status code 200", async () => {
+      await request(server)
+        .post("/api/games")
+        .send(videogame1);
+
+      const response = await request(server).get("/api/games");
+
+      expect(response.status).toBe(200);
+    });
+
+    it("should always return an array even if no videogames stored", async () => {
+      const response = await request(server).get("/api/games");
+
+      expect(response.body).toEqual([]);
     });
   });
 });
